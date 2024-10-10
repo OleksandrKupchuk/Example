947c9c58d2206ec1ba8b3fd7e152297124838caa
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2024-10-10 15:45:09 +0300

    comment Initialization

--- src/test/java/Initialization.java
+++ src/test/java/Initialization.java
@@ -5,22 +5,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class Initialization {
-    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    public WebDriver getDriver(){return driver.get();}
-
-    @BeforeMethod
-    public void setup(){
-//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\chromedriver.exe");
-        driver.set(new ChromeDriver());
-    }
-
-    @AfterMethod
-    public void teardown(){
-        driver.get().quit();
-    }
-
-    @AfterClass
-    public void terminate(){
-        driver.remove();
-    }
+//    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+//    public WebDriver getDriver(){return driver.get();}
+//
+//    @BeforeMethod
+//    public void setup(){
+////        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\chromedriver.exe");
+//        driver.set(new ChromeDriver());
+//    }
+//
+//    @AfterMethod
+//    public void teardown(){
+//        driver.get().quit();
+//    }
+//
+//    @AfterClass
+//    public void terminate(){
+//        driver.remove();
+//    }
 }
